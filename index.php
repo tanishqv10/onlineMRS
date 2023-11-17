48a1b0649a99e84d3f8b72864e99119e5f391acd
tanishqv10 <tvarshne@usc.edu>
2023-11-16 20:24:21 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,308 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+    function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!--//Meta tag Keywords -->
+    
+    <!--stylesheet-->
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <!--font_aswesome_icons-->
+    <link href="//fonts.googleapis.com/css?family=Basic" rel="stylesheet">
+    <!--online-fonts-->
+    <link href="//fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+    <!--online-fonts-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="images/logo.jpg" type="image/png">
+    <title>Online Marriage Portal by MIT-WPU Students :: Home Page</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="vendors/animate-css/animate.css">
+    <link rel="stylesheet" href="vendors/popup/magnific-popup.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/responsive.css">
+	<script src="https://kit.fontawesome.com/yourcode.js"></script>
+</head>
+<body>
+
+    <!--================ Start header Top Area =================-->
+    <section class="header-top" >
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-6 col-lg-4">
+                    <div class="float-left">
+                        <ul class="header_social">
+                            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        </ul>   
+                    </div>
+                </div>
+                <div class="col-6 col-lg-4 col-md-6 col-sm-6 logo-wrapper">
+                    <a href="index.html" class="logo">
+                        <img src="img/logo.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 search-trigger">
+                    <div class="right-button">
+                        <ul>
+                            
+                            <li><a href="admin/login.php">Admin</a></li>
+                            <li><a href="user/login.php">User</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+    <!--================ End header top Area =================-->
+
+   
+
+    <!--================Fullwidth block Area =================-->
+
+    <section class="fullwidth-block area-padding-bottom">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 col-lg-6 col-xl-5">
+                    <div class="single-blog">
+                        <div class="thumb">
+                            <img class="img-fluid" src="images/img1.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Online Marriage Registration Portal</h4>
+                            </a>
+                            
+                        </div>
+                    </div>    
+                </div>
+
+                <div class="col-md-6 col-lg-6 col-xl-4">
+                    <div class="single-blog style_two">
+                        <div class="thumb">
+                            <img class="img-fluid" src="images/img.jpg" alt="">
+                        </div>
+                        <div class="short_details text-center ">
+
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Handling the marriage process offline can be quite time-consuming. Register your marriage here!</h4>
+                            </a>
+                        </div>
+                    </div>    
+                </div>
+
+                <div class="col-md-6 col-lg-6 col-xl-3">
+                    <div class="single-blog style_two">
+                        <div class="thumb">
+                            <img class="img-fluid" src="images/img2.jpg" alt="">
+                        </div>
+                        <div class="short_details text-center ">
+                
+                            <a class="d-block" href="user/login.php">
+                                <h4 >Apply Here.</h4>
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!--================Fullwidth block Area end =================-->
+	<section class="first_block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-xl-6">
+                    <div class="single-blog row no-gutters style-four border_one">
+                        <div class="col-12 col-sm-5">
+                            <div class="thumb">
+                                <img class="img-fluid" src="images/m1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-12 col-sm-7">
+                            <div class="short_details">
+                                <div class="meta-top d-flex">
+                                    <a href="#">About Us</a>
+                                </div>
+                                <a class="d-block" href="single-blog.html">
+                                    <h4 style="font-size:15px;">Marriage registration is one of the most important tasks a marriage certificate is needed to for the proof.
+						A registration helps both partners in a marriage, either before, with issues during, or if a spouse passes away. </h4>
+                                </a>
+                                
+                            </div>  
+                        </div>  
+                    </div>      
+                </div>
+
+                <div class="col-lg-8 col-xl-6">
+                    <div class="single-blog style_five">
+                            <div class="thumb" >
+                            <img class="img-fluid" src="images/m3.jfif" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#" style="font-size:20px;">Benefits of marriage certificate</a>
+                                
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <p >- If you are applying for a passport or opening a bank account post the wedding, a Marriage Certificate is required and comes in handy.</p>
+							<p >- It is extremely helpful in obtaining visas for both, the husband and wife.</p>
+							<p >- As foreign embassies in India as well as countries outside India, do not recognize traditional marriages, the Marriage Certificate is mandatory for the couple travelling abroad on a spouse visa.</p>
+							<p >- Make the process of claiming life insurance return or bank deposits in case of demise of the Insurer or depositor without any nominee easier.</p>
+							</a>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+               
+            </div>
+        </div>
+    </section>
+	
+    <!--================ three-block section start ================= -->
+
+    <div class="three-block  area-padding">
+        <div class="container">
+            <div class="row">
+                <div class="area-heading">
+                    <h3>Features</h3>
+                    <p>Get your marriage certificate within hours!</p>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="single-blog style-five">
+                        <div class="thumb">
+                            <img class="img-fluid" src="images/reg.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">Marriage Registration</a>
+                                
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Register for your marriage.</h4>
+                            </a>
+                            
+                        </div>
+                    </div> 
+
+                </div> 
+
+                <div class="col-lg-4">
+                    <div class="single-blog style-five">
+                        <div class="thumb">
+                            <img class="img-fluid" src="images/user.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">User Dashboard (Better GUI) </a>
+                               
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>User friendly</h4>
+                            </a>
+                            
+                        </div>
+                    </div> 
+
+                </div> 
+
+                <div class="col-lg-4">
+                    <div class="single-blog style-five">
+                        <div class="thumb">
+                            <img class="img-fluid" src="images/cert.jpg" alt="">
+                        </div>
+                        <div class="short_details">
+                            <div class="meta-top d-flex">
+                                <a href="#">Generate marriage certificate</a>
+                                
+                            </div>
+                            <a class="d-block" href="single-blog.html">
+                                <h4>Certificate provided with government verification.</h4>
+                            </a>
+                           
+                        </div>
+                    </div> 
+                </div> 
+            </div>
+        </div>
+    </div> 
+
+
+    <!-- ================ start footer Area ================= -->
+    <footer class="footer-area" style="background-image: url(images/dest.jpg);background-repeat: no-repeat;background-position: center;background-size: cover;">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4 style="color:#ffffff;">About Us</h4>
+                    <p style="color:#ffffff;">We provide professional form filing and homepostage customer advice and help with Applications for Marriage 
+						services by professional administration team oversean by a High Court Advocate.</p>
+                    <div class="footer-logo">
+                        <img src="img/logo.png" alt="">
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-sm-6 mb-4 mb-xl-0 single-footer-widget">
+                    <h4 style="color:#ffffff;">Contact Info</h4>
+                    <div class="footer-address">
+                        <p style="color:#ffffff;">Address : MIT-WPU, Pune.</p>
+                        <span style="color:#ffffff;">Phone : +91 9999999999</span>
+                        <span style="color:#ffffff;">Email : wtl@gmail.com</span>
+                    </div>
+                </div>
+
+            </div>
+        
+        <div class="footer-bottom row align-items-center text-center text-lg-left no-gutters">
+            <p class="footer-text col-md-12" style="text-align:center;color:#ffffff;">
+             Copyright &copy;<script>document.write(new Date().getFullYear());</script>. Online Marriage Registration System </a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+<p class="footer-text col-md-12" style="text-align:center;color:#ffffff;">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+</p>
+        </div>
+    </div></div>
+</footer>
+<!-- ================ End footer Area ================= -->
+
+
+
+
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/stellar.js"></script>
+<script src="vendors/popup/jquery.magnific-popup.min.js"></script>
+<script src="js/jquery.ajaxchimp.min.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/mail-script.js"></script>
+<script src="js/contact.js"></script>
+<script src="js/jquery.form.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/mail-script.js"></script>
+<script src="js/theme.js"></script>
+</body>
+</html>
